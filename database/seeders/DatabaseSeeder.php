b8885e4673ebc5098d7357b19e13e70ad652dbb5
Ewelina Jankowska <ewelina.jankowska@outlook.com>
2021-05-07 13:22:40 +0200

    add book seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Book;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Book::truncate();
+
+        Book::create([
+            'isbn' => "9789178871858",
+            'title' => "Städa hållbart med Ekotipset: husmorsknep och ekohacks",
+            'author' => "Ellinor Sirén",
+            'image' => "https://s2.adlibris.com/images/59920084/stada-hallbart-med-ekotipset-husmorsknep-och-ekohacks.jpg",
+        ]);
+
+        Book::create([
+            'isbn' => "9789178872121",
+            'title' => "Jennys sommar",
+            'author' => "Jenny Warsén",
+            'image' => "https://s1.adlibris.com/images/60180380/jennys-sommar.jpg",
+        ]);
+
+        Book::create([
+            'isbn' => "9789178870417",
+            'title' => "Wok, ris, nudlar",
+            'author' => "Jennie Walldén",
+            'image' => "https://s1.adlibris.com/images/57918624/wok-ris-nudlar.jpg",
+        ]);
     }
 }
